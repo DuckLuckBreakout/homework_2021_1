231f4b21dc3adab7a3ddcfca3fb18c425fe08953
DuckLuckBreakout <mrdivan07@gmail.com>
2021-02-06 23:28:18 +0300

    Leave only 2 main tests

--- source/minmax.js
+++ source/minmax.js
@@ -6,45 +6,13 @@
  *
  * @author Ivan Kovalenko <mrdivan07@gmail.com>
  *
- * @example <caption>minmax works correctly on strings without numbers: </caption>
- * // returns [ undefined, undefined ]
- * minmax('lala la la');
- *
- * @example <caption>min max correctly parses individual numbers: </caption>
- * // returns [ Infinity, Infinity ]
- * minmax('Infinity');
- *
- * @example
- * // returns [ 1.1, 1.1 ]
- * minmax('1.1');
- *
- * @example
- * // returns [ .01, .01 ]
- * minmax('.01');
- *
- * @example
- * // returns [ 1e5, 1e5 ]
- * minmax('1e5');
- *
- * @example
- * // returns [ -.1e-5, -.1e-5 ]
- * minmax('-.1e-5');
- *
  * @example <caption>minmax correctly parses multiple numbers:</caption>
  * // returns [ 1, 4 ]
  * minmax('1 2 3 4');
  *
- * @example
- * // returns [ -Infinity, Infinity ]
- * minmax('-Infinity -1 0 1 Infinity');
- *
- * @example <caption>min max ignores text: </caption>
+ * @example <caption>minmax ignores text: </caption>
  * // returns [ -5.8, 73 ]
  * minmax('1, -5.8 lala 10, qwer 34 + -5.3 s 73');
- *
- * @example <caption>min max ignores invalid data types: </caption>
- * // returns [ undefined, undefined ]
- * minmax(1);
  */
 const minmax = (str = '') => {
     if (typeof(str) != 'string')
