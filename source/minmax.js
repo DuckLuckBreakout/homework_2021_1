e4127160f3e729db6f7dc49b02e57da63f8caf13
DuckLuckBreakout <mrdivan07@gmail.com>
2021-02-06 21:00:35 +0300

    Fix code style

--- source/minmax.js
+++ source/minmax.js
@@ -104,10 +104,9 @@
  */
 
 
-
-const minmax = (str='') => str.split(' ').map(x => parseFloat(x)).filter(x => !isNaN(x)).reduce(
+const minmax = (str = '') => str.split(' ').map(x => parseFloat(x)).filter(x => !isNaN(x)).reduce(
     ([min = Infinity, max = -Infinity], number) => [
         min < number ? min : number,
         max > number ? max : number
     ], [undefined, undefined]
-)
+);
